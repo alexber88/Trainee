fc07250f4e4a19f72d8ebe2ff937ec52c0297a36
Alex <a.bereza88@gmail.com>
2016-08-03 10:08:48 +0300

    last changes

--- magento/local/Alex/UpdatePrice/Model/Price.php
+++ magento/local/Alex/UpdatePrice/Model/Price.php
@@ -37,7 +37,7 @@ class Alex_UpdatePrice_Model_Price extends Mage_Core_Model_Abstract
         }
         catch(Exception $e)
         {
-            Mage::getSingleton('adminhtml/session')->addException($e, $helper->__('Error during product updating'));
+            Mage::getSingleton('adminhtml/session')->addException($e->getMessage());
         }
 
     }
